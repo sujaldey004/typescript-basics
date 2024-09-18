a486012be6ada062df99d71c25b0d6f10570e0ad
sujaldey004 <sujaldey2920@gmail.com>
2024-09-19 00:49:47 +0530

    Generics in ts

--- src/a.ts
+++ src/a.ts
@@ -1,18 +1,8 @@
-enum Direction {
-    Up = 1,
-    Down = 10,
-    Left = 40,
-    Right
+function play<T>(arg:T[]): T{
+    return arg[0];
 }
 
-console.log(Direction.Up)
-console.log(Direction.Down)
-console.log(Direction.Left)
-console.log(Direction.Right)
-
-
-// output : 
-// 1
-// 10
-// 40
-// 41
+const value = play(["sam", "sujal"])
+const anotherValue = play([1, 2])
+console.log(value.toUpperCase());
+console.log(anotherValue);
