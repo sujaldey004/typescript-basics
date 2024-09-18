4cf39aa49bbc3878c36e16c812c5176647a32ab0
sujaldey004 <sujaldey2920@gmail.com>
2024-09-18 23:21:38 +0530

    UseCase-2 in enum

--- src/a.ts
+++ src/a.ts
@@ -1,11 +1,18 @@
 enum Direction {
-    Up = "up",
-    Down = "down",
-    Left = "left",
-    Right = 'right'
+    Up = 1,
+    Down = 10,
+    Left = 40,
+    Right
 }
 
 console.log(Direction.Up)
 console.log(Direction.Down)
 console.log(Direction.Left)
 console.log(Direction.Right)
+
+
+// output : 
+// 1
+// 10
+// 40
+// 41
