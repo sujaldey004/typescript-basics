37591b7ebc69cacfb724590193fb7a5524ac6377
sujaldey004 <sujaldey2920@gmail.com>
2024-09-18 20:20:54 +0530

    interface in ts

--- src/a.ts
+++ src/a.ts
@@ -1,10 +1,21 @@
-// Basic ts file
-function play(fn:()=>void, timeout:number){
-    setTimeout(()=>{
-        fn()
-    }, timeout);
+interface User{
+    firstName : String,
+    lastName : String,
+    age : number
 }
 
-play(()=>{
-    console.log("Hello Sam");
-}, 1000)
+function play(user : User){
+    if(user.age > 18 || user.age == 18){
+        return true;
+    }
+
+    return false;
+}
+
+const value = play({
+    firstName: "Sujal",
+    lastName: "Dey",
+    age: 20
+})
+
+console.log(value);
