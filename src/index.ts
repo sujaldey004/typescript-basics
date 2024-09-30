27e82ad18b388d1bbe9cd085ad18559799ada393
sujaldey004 <sujaldey2920@gmail.com>
2024-09-30 19:52:26 +0530

    Readonly api

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-interface user {
+// type user = {
+//     name : string,
+//     age : number
+// }
+
+// const userProp : user = {
+//     name : "sam",
+//     age : 20
+// }
+
+// userProp.age = 21;  // we can change props of object and arrays like this 
+
+// console.log(userProp.age);
+
+type user = {
     name : string,
     age : number
-    email : string,
-    address: string
 }
 
-type userProps = Pick<user, 'age' | 'name'>
-
-
-type userPropsOptional = Partial<userProps>
-
-function SumOfAge (user1 : userPropsOptional, user2 : userPropsOptional){
-    const age1 = user1.age ?? 0;
-    const age2 = user2.age ?? 0;
-    return age1 + age2;
+const userProp : Readonly<user> = {
+    name : 'sam',
+    age : 20
 }
 
-const result = SumOfAge({age: 10}, {age: 20})
-console.log(result);
+userProp.age(21); // Now we can't change this because we used Readonly api here
+console.log(userProp)
